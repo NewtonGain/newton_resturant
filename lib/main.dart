c1a334e504e54cfb2a2d6e4cd49cf96a82de6cb9
NewtonGain <newtongain7@gmail.com>
2022-01-03 11:20:43 +0600

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:newton_resturant/home.dart';
 
-void main()=>runApp(const MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -13,40 +13,61 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      
-      appBar: AppBar(
-        
-        centerTitle: true,
-        
-      title: 
-      
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [Icon(Icons.restaurant), 
-      SizedBox(width: 13,),
-      Text("Newton Resturant")],),
-      
-       ),
-       drawer: Drawer(backgroundColor: Colors.teal,child:
-        ListView(children:[
-         const DrawerHeader(child: Center(child: Text("Help Line",
-         style: TextStyle(fontSize: 30),)),
-         decoration: BoxDecoration(color: Colors.amberAccent ),
-         ),
-         ListTile(leading: const Icon(Icons.person),title: const Text("Newton"),
-         onTap: (){},
-         ),
-          ListTile(leading: const Icon(Icons.phone),title: const Text("01918104119"),
-          onTap: (){},
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.restaurant_menu,
+                  color: Colors.black,
+                ),
+                Text("Newton Resturant")
+              ],
+            ),
           ),
-           ListTile(leading: const Icon(Icons.email),title: const Text("newtongain7@gmail.com"),
-           onTap: (){},
-           ),
-            
-       ],),),
-       body: const HomePage()
-     ),
+          drawer: Drawer(
+            backgroundColor: Colors.teal,
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  child: Column(
+                    children: const [
+                      CircleAvatar(
+                        backgroundColor: Colors.red,
+                        radius: 59,
+                      ),
+                      Text(
+                        "Newton Resturant",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  decoration: const BoxDecoration(color: Colors.green),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: const Text("Newton"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.phone),
+                  title: const Text("01918104119"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.email),
+                  title: const Text("newtongain7@gmail.com"),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          body: const HomePage()),
     );
   }
 }
